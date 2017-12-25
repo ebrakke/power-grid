f598c1bc64a7272d5f0eb39f3bd1d46346a0e318
Erik Brakke <eriksbrakke@gmail.com>
2017-12-25 12:21:40 -0500

    Made reducers and actions, updated some tests (#8)
    * Made reducers and actions, updated some tests
    
    * pr updates

--- src/reducers/game.py
+++ src/reducers/game.py
@@ -1,11 +1,20 @@
+from copy import deepcopy
+
+
 def initial_game():
     """ setup an intial game"""
     return dict(
-        turn=0,
-        phase=0,
-        current_bid=None,
-        player_order=[],
-        current_player=None
+        phase=1,
+        step=1,
+        current_player='',
+        current_bid=dict(
+            player_id='',
+            card=None,
+            amount=0,
+            passed=[]
+        ),
+        player_rank=[],
+        bought_or_passed=[]
     )
 
 
@@ -15,46 +24,73 @@ def game(state=None, action=None):
     if action is None:
         return state
 
-    ty = action.get('type')
-    if ty == 'NEXT_TURN':
-        next_state = dict(**state)
-        next_state['turn'] = (state['turn'] + 1) % 3
+    action_type = action.get('type')
+    if action_type == 'GAME_NEXT_STEP':
+        if state['step'] == 3:
+            return state
+        next_state = deepcopy(state)
+        next_state['step'] = state['step'] + 1
         return next_state
 
-    if ty == 'NEXT_PHASE':
-        next_state = dict(**state)
-        next_state['phase'] = (state['phase'] + 1) % 4
+    if action_type == 'GAME_NEXT_PHASE':
+        next_state = deepcopy(state)
+        next_state['phase'] = (state['phase'] % 5) + 1
         return next_state
 
-    if ty == 'BID_ON_POWER_PLANT':
-        next_state = dict(**state)
+    if action_type == 'GAME_SET_CURRENT_BID':
+        next_state = deepcopy(state)
         # only update the bid if the new amount is greater than the old amount
         if state.get('current_bid') is None or state.get('current_bid').get('amount') < action.get('amount'):
             next_state['current_bid'] = dict(
                 player_id=action.get('player_id'),
                 card=action.get('card'),
-                amount=action.get('amount')
+                amount=action.get('amount'),
+                passed=state.get('current_bid').get('passed')
             )
         return next_state
 
-    if ty == 'CREATE_PLAYER':
-        next_state = dict(**state)
-        next_state['player_order'].append(action.get('player_id'))
-
-        # Set current player if none is set yet
-        if state.get('current_player') is None:
-            next_state['current_player'] = action.get('player_id')
+    if action_type == 'GAME_PASS_CURRENT_BID':
+        next_state = deepcopy(state)
+        next_state['current_bid']['passed'] = state['current_bid']['passed'] + \
+            [action.get('player_id')]
         return next_state
 
-    if ty == 'NEXT_PLAYER':
-        current_player_index = state.get(
-            'player_order').index(state.get('current_player'))
-        # This will just be the next player in the player_order list
-        next_player_index = (current_player_index +
-                             1) % len(state.get('player_order'))
-        next_state = dict(**state)
-        next_state['current_player'] = state.get('player_order')[
-            next_player_index]
+    if action_type == 'GAME_PASS_INITIAL_BID':
+        next_state = deepcopy(state)
+        next_state['bought_or_passed'] = state['bought_or_passed'] + \
+            [action.get('player_id')]
+        return next_state
+
+    if action_type == 'GAME_WIN_CURRENT_BID':
+        next_state = deepcopy(state)
+        # Add the player with the highest bid to the bought_or_passed property
+        next_state['bought_or_passed'] = state['bought_or_passed'] + \
+            [state['current_bid']['player_id']]
+        return next_state
+
+    if action_type == 'GAME_CLEAR_BID':
+        next_state = deepcopy(state)
+        next_state['current_bid'] = dict(
+            player_id='',
+            card=None,
+            amount=0,
+            passed=[]
+        )
+        return next_state
+
+    if action_type == 'GAME_SET_PLAYER_RANK':
+        next_state = deepcopy(state)
+        next_state['player_rank'] = action.get('player_rank')
+        return next_state
+
+    if action_type == 'GAME_SET_CURRENT_PLAYER':
+        next_state = deepcopy(state)
+        next_state['current_player'] = action.get('player_id')
+        return next_state
+
+    if action_type == 'GAME_CLEAR_BOUGHT_OR_PASSED':
+        next_state = deepcopy(state)
+        next_state['bought_or_passed'] = []
         return next_state
 
     return state
