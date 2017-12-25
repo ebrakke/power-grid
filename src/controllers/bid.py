75b837a6d7b8d35b1d02228c8678ef2a5f6bfd27
kbrakke <kbrakke@gmail.com>
2017-12-25 10:25:44 -0700

    Initial bid validation

--- src/controllers/bid.py
+++ src/controllers/bid.py
@@ -8,20 +8,33 @@ def bid(data, store):
     card = data.get('card')
 
     state = store['get_state']()
-    current_bid = state.get('game').get('current_bid')
-    # All of these fields must be present
-    if not all([player_id, amount, card]):
-        return
+    if not valid_bid(player_id, amount, card, state):
+        return False
 
-    # This is not a valid card to bid on
-    if card not in state.get('market').get('current'):
-        return
 
-    # Not allowed to bid on another card if a bid is currently going
-    if current_bid and current_bid.get('card') != card:
-        return
+
 
     action = bid_on_power_plant(player_id, card, amount)
     store['dispatch'](action)
     store['dispatch'](next_player())
-    return
+    return True
+
+def valid_bid(player_id, amount, card, state):
+    current_bid = state.get('game').get('current_bid')
+    # All of these fields must be present
+    if not all([player_id, amount, card]):
+        return False
+
+    # This is not a valid card to bid on
+    if card not in state.get('market').get('current'):
+        return False
+
+    # Not allowed to bid on another card if a bid is currently going
+    if current_bid and current_bid.get('card') != card:
+        return False
+
+    player = [player for player in state.get('players') if player['player_id'] == player_id][0]
+    # You can't bid more than you have
+    if amount > player['money']:
+        return False
+
