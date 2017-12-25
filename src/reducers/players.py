3f994cba496afa78259c2bb39c680354c21ec40b
Erik Brakke <ebrakke@carbonite.com>
2017-12-25 15:37:35 -0700

    added player actions

--- src/reducers/players.py
+++ src/reducers/players.py
@@ -23,13 +23,13 @@ def players(state=None, action=None):
         next_state = deepcopy(state)
         for p in next_state:
             if p == action.get('player_id'):
-                p['money']+= action.get('money')
+                p['money'] += action.get('money')
         return next_state
     if action_type == 'PLAYER_ADD_POWER_PLANT':
         next_state = deepcopy(state)
         for p in next_state:
             if p == action.get('player_id'):
-                if len(p['power_plants'])< 4:
+                if len(p['power_plants']) < 4:
                     p['power_plants'].append('power_plant')
         return next_state
     if action_type == 'PLAYER_REMOVE_POWER_PLANT':
@@ -40,8 +40,5 @@ def players(state=None, action=None):
                     p['power_plants'].remove('card')
 
         return next_state
-
-
-
 
     return state
