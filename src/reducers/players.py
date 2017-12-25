6b6feb2e3114f5fdc66543e51bce54164fa1e574
nelsp <nelspbrakke@gmail.com>
2017-12-25 13:38:50 -0700

    player-reducer updated

--- src/reducers/players.py
+++ src/reducers/players.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 def create_player(player_id):
     """ Returns a new player for the game
     :param name
@@ -15,8 +18,30 @@ def players(state=None, action=None):
     if action is None:
         return state
 
-    ty = action.get('type')
-    if ty == 'CREATE_PLAYER':
-        return state + [create_player(action.get('player_id'))]
+    action_type = action.get('type')
+    if action_type == 'PLAYER_CHANGE_MONEY':
+        next_state = deepcopy(state)
+        for p in next_state:
+            if p == action.get('player_id'):
+                p['money']+= action.get('money')
+        return next_state
+    if action_type == 'PLAYER_ADD_POWER_PLANT':
+        next_state = deepcopy(state)
+        for p in next_state:
+            if p == action.get('player_id'):
+                if len(p['power_plants'])< 4:
+                    p['power_plants'].append('power_plant')
+        return next_state
+    if action_type == 'PLAYER_REMOVE_POWER_PLANT':
+        next_state = deepcopy(state)
+        for p in next_state:
+            if p == action.get('player_id'):
+                if len(p['power_plants']) > 3:
+                    p['power_plants'].remove('card')
+
+        return next_state
+
+
+
 
     return state
